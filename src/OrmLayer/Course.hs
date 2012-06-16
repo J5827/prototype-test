fedec019764cdf84acad60d646d9d46187e5221e
J5827 <jerber1@imn.htwk-leipzig.de>
2012-06-16 18:55:24 +0100

    Remove unneeded file.

--- src/OrmLayer/Course.hs
+++
@@ -1,6 +0,0 @@
-------------------------------------------------------------------------------
--- | This module (group) is just an idea, maybe not even necessary, but maybe
--- it is.
-module OrmLayer.Course
-    ( getCourses
-    ) where
